b5053fb0b1a12767c07331d0836a76a30350582c
CGS-Matthew-Purcell <matthew.purcell@cgs.act.edu.au>
2018-08-21 09:35:22 +1000

    End of lesson 21 Aug

--- Source Code/Pong/SplashScene.swift
+++ Source Code/Pong/SplashScene.swift
@@ -10,13 +10,47 @@ import SpriteKit
 
 class SplashScene: SKScene {
 
+    var touchedInsideButton = false
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        let transition = SKTransition.crossFade(withDuration: 1.0)
-        let gameScene = SKScene(fileNamed: "GameScene")!
-        gameScene.scaleMode = .aspectFill
-        scene!.view!.presentScene(gameScene, transition: transition)
+        let touch = touches.first!
+        let touchLocation = touch.location(in: self)
+        let touchedNode = atPoint(touchLocation)
         
+        if touchedNode.name == "buttonNode" {
+            
+            touchedInsideButton = true
+            let buttonNode = touchedNode as! SKSpriteNode
+            buttonNode.color = .blue
+            
+        }
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let touch = touches.first!
+        let touchLocation = touch.location(in: self)
+        let touchedNode = atPoint(touchLocation)
+        
+        if touchedNode.name == "buttonNode" {
+            
+            if touchedInsideButton {
+            
+                let transition = SKTransition.crossFade(withDuration: 1.0)
+                let gameScene = SKScene(fileNamed: "GameScene")!
+                gameScene.scaleMode = .aspectFill
+                scene!.view!.presentScene(gameScene, transition: transition)
+                
+            }
+            
+        }
+        
+        touchedInsideButton = false
+        let buttonNode = childNode(withName: "buttonNode") as! SKSpriteNode
+        buttonNode.color = .red
+   
     }
     
 }
